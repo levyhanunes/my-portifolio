5856a89d77642cb7710380ed8f515db2c24aed35
Levy Nuunes <levy.nunes@SQD-WKS-320.local>
2023-01-07 14:13:19 -0300

    github upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles'
-
+import Image from 'next/image'
+import HeroImage from '../public/hero-image.jpeg'
 
 type Props = {}
 
@@ -14,10 +15,23 @@ export default function Hero({}: Props) {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircles />
-        <h1>
-            <span>{text}</span>
+        <Image
+            className='relative rounded-full h-32 w-32 mx-auto object-cover'
+            src={HeroImage}
+            alt=""
+        />
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+            <span className='mr-3'>{text}</span>
             <Cursor cursorColor='#f7ab0a'/>
         </h1>
+
+        <div>
+            <button className='heroButton'>About</button>
+            <button className='heroButton'>Experience</button>
+            <button className='heroButton'>Skills</button>
+            <button className='heroButton'>Projects</button>
+        </div>
     </div>
   )
 }
