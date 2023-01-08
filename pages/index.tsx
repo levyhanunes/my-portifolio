e1a2168f3daee661ba3d6934be5fb21b148ec95b
Levy Nuunes <levy.nunes@SQD-WKS-320.local>
2023-01-07 21:41:00 -0300

    skills section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '../components/Header'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
+import Skills from '../components/Skills'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -32,8 +33,14 @@ export default function Home() {
       </section>
 
       {/* Skills */}
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
 
       {/* Projects */}
+      {/* <section id='projects' className='snap-start'>
+        <Projects />
+      </section> */}
 
       {/* Contact Me */}
 
