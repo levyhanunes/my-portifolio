0f8d4299d6e48634a8325513a6515a2a1e38a8b4
Levy Nuunes <levy.nunes@SQD-WKS-320.local>
2023-01-07 23:20:18 -0300

    fix footer object cover

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         <footer className='sticky bottom-5 w-full cursor-pointer'>
           <div className="flex items-center justify-center">
             <Image
-              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 object-contain'
+              className='h-10 w-10 rounded-full filter grayscale hover:grayscale-0 object-cover'
               src={HeroImage}
               alt=""
             />
